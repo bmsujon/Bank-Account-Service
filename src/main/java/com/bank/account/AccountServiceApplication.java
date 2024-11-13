3f35528cf520fad79a208946597cb2fa38823349
Wahidul Azam <wahidulazam@Wahiduls-MacBook-Pro.local>
2024-11-14 00:13:10 +0600

    api doc updated

--- src/main/java/com/bank/account/AccountServiceApplication.java
+++ src/main/java/com/bank/account/AccountServiceApplication.java
@@ -7,8 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(info = @Info(
 		title = "Account service API",
 		version = "v1",
-		description = "Account service related api documentation"
-))
+		description = """
+			# Account service related api documentation
+			## Steps to create a bank account and get bank account details, user details etc.:
+			1. Create an user with user/register api. You will get user object in response
+			2. Login with userName with password of your user. You will get a token
+			3. For all other api endPoints you will have to use Authorization header and put the token you have got in step 2
+				
+			*** Note: If you want to test the endpoints with postman, then you can collect the curl by requesting the endpoint here once, and then use that curl in your postman for testing.
+				"""))
 @SpringBootApplication
 public class AccountServiceApplication {
 
